b0beba52e702b6d4cdadc5edc079a66f88523c89
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2025-06-20 21:10:28 -0700

    Update Grid_Tradeskills.lua
    Updated the list of available expansions to choose from

--- Altoholic_Grids/Grid_Tradeskills.lua
+++ Altoholic_Grids/Grid_Tradeskills.lua
@@ -7,12 +7,16 @@ local LCI = LibStub("LibCraftInfo-1.0")
 local LCL = LibStub("LibCraftLevels-1.0")
 
 local ICON_QUESTIONMARK = "Interface\\RaidFrame\\ReadyCheck-Waiting"
+local isMists = LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_MISTS_OF_PANDARIA
 
 local xPacks = {
 	EXPANSION_NAME0,	-- "Classic"
 	EXPANSION_NAME1,	-- "The Burning Crusade"
-	EXPANSION_NAME2,	-- "Wrath of the Lich King"													
+	EXPANSION_NAME2,	-- "Wrath of the Lich King"
+	EXPANSION_NAME3,	-- "Cataclysm"
+	--EXPANSION_NAME4,	-- "Mists of Pandaria"
 }
+if isMists then table.insert(xPacks, EXPANSION_NAME4) end
 
 local OPTION_XPACK = "Tradeskills.CurrentXPack"
 local OPTION_TRADESKILL = "Tradeskills.CurrentTradeSkill"
